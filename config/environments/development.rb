54c79d8bf83f6b6072c71ab41ba14cee2e3abe99
reversonthaiam <reversonthayan@gmail.com>
2026-03-21 18:54:37 -0300

    fix: change mailer password

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     user_name: "cda0c82868b741",
-    password: "****4a4a",
+    password: "46f9ff7de84a4a",
     address: "sandbox.smtp.mailtrap.io",
     host: "sandbox.smtp.mailtrap.io",
     port: "2525",
